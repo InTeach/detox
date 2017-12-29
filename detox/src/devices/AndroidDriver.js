2ef6efb78b3f3246128afbea859970f0e4365674
Quentin Valmori <quentin.valmori@gmail.com>
2017-12-29 15:18:37 +0100

    Update AndroidDriver.js

--- detox/src/devices/AndroidDriver.js
+++ detox/src/devices/AndroidDriver.js
@@ -126,8 +126,9 @@ class AndroidDriver extends DeviceDriverBase {
 
   async findDeviceId(filter) {
     const adbDevices = await this.adb.devices();
+    console.log('adbDevices', adbDevices);
     const filteredDevices = _.filter(adbDevices, filter);
-
+    console.log('filter', filter);
     let adbName;
     switch (filteredDevices.length) {
       case 1:
@@ -135,11 +136,11 @@ class AndroidDriver extends DeviceDriverBase {
         adbName = adbDevice.adbName;
         break;
       case 0:
-        throw new Error(`Could not find '${name}' on the currently ADB attached devices, 
+        throw new Error(`Could not find '${filter}' on the currently ADB attached devices, 
       try restarting adb 'adb kill-server && adb start-server'`);
         break;
       default:
-        throw new Error(`Got more than one device corresponding to the name: ${name}`);
+        throw new Error(`Got more than one device corresponding to the name: ${filter}`);
     }
 
     return adbName;
